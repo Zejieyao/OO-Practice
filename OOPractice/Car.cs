b848c0cb74af22eb4bfd6bdff70abb1996d6f185
Zejie Yao <ZYao3@slb.com>
2022-10-27 11:27:58 +0800

    feat: add Should_return_message_when_speed_up_gien_car_name_and_speed test and function

---
+++ OOPractice/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OOPractice
+{
+    public class Car
+    {
+        private string name;
+        private int speed;
+
+        public Car(string name, int speed)
+        {
+            this.name = name;
+            this.speed = speed;
+        }
+
+        public string SpeedUp()
+        {
+            return $"{this.name}: speed up {this.speed} km/h";
+        }
+    }
+}
---
+++ OOPracticeTest/CarTest.cs
@@ -0,0 +1,24 @@
+﻿using OOPractice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OOPracticeTest
+{
+    public class CarTest
+    {
+        [Fact]
+        public void Should_return_message_when_speed_up_gien_car_name_and_speed()
+        {
+            //given
+            Car car = new Car("Cool Car", 30);
+            //when
+            string msg = car.SpeedUp();
+            //then
+            Assert.Equal("Cool Car: speed up 30 km/h", msg);
+        }
+    }
+}
